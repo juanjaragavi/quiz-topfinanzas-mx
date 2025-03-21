ea9bb824b111231c7d11a56a0cf808eb452380d9
root <root@topfinanzas-com.us-central1-a.c.absolute-brook-452020-d5.internal>
2025-03-21 15:24:11 +0000

    Update bun.lockb and add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "quiz-topfinanzas-mx",
+      script: "node_modules/.bin/next",
+      args: "start",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3002
+      }
+    }
+  ]
+};
