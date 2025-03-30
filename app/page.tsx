61b7376b8ab65f4b6ecb2e6d388b39db4fa2799e
Juan Jaramillo <info@juanjaramillo.tech>
2025-03-30 13:15:49 -0500

    fix: corregir error tipográfico en la clase CSS del componente principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CreditCardForm from "@/components/credit-card-form"
 
 export default function Home() {
   return (
-    <main className="flex min-h-[100dvh] fleFx-col justify-start bg-gray-100 pb-safe">
+    <main className="flex min-h-[100dvh] flex-col justify-start bg-gray-100 pb-safe">
       <section className="w-full p-0">
         <CreditCardForm />
       </section>
